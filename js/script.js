5c8e1dd5379d74b26bf0fb0fc50c0f7a6cd612b1
diana-tokach <m.diana.tokach@gmail.com>
2020-05-13 00:49:22 +0300

    added second task
    events

--- js/script.js
+++ js/script.js
@@ -1,49 +1,90 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded' , () => {
 
-let promoBlocks = document.querySelector('.promo__adv'),
-      promoImgs = promoBlocks.querySelectorAll('img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      moviesListBlock = document.querySelector('.promo__interactive-list');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+    let promoBlocks = document.querySelector('.promo__adv'),
+        promoImgs = promoBlocks.querySelectorAll('img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        moviesListBlock = document.querySelector('.promo__interactive-list'),
+        addFilmForm = document.querySelector('.add');
 
-promoImgs.forEach(item => {
-    item.remove();
-});
+    promoImgs.forEach(item => {
+        item.remove();
+    });
 
-// 2) Изменить жанр фильма, поменять "комедия" на "драма"
+    genre.textContent = 'Драма';
 
-genre.textContent = 'Драма';
+    poster.style.background = 'url("../img/bg.jpg") top center/cover no-repeat';
 
-// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-// Реализовать только при помощи JS
+    const sortArray = (arr) => {
+        arr.sort();
+    };
 
-poster.style.background = 'url("../img/bg.jpg") top center/cover no-repeat';
+    const showAllMovies = (films, parent) => {
+        parent.innerHTML = '';
 
-// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-// Отсортировать их по алфавиту
+        sortArray(movieDB.movies);
+        films.forEach((item, i) => {
+            parent.insertAdjacentHTML('beforeend', 
+                `<li class="promo__interactive-item">
+                    ${i + 1}. ${item}
+                    <div class="delete"></div>
+                </li>`);
+        });
 
-// 5) Добавить нумерацию выведенных фильмов
+        // 3) При клике на мусорную корзину - элемент будет удаляться из списка
+            
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                showAllMovies(movieDB.movies, moviesListBlock);
+            });
+        });
+    };
 
-moviesListBlock.innerHTML = '';
+    showAllMovies(movieDB.movies, moviesListBlock);
 
-movieDB.movies.sort();
+    // Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+    // новый фильм добавляется в список. Страница не должна перезагружаться.
+    // Новый фильм должен добавляться в movieDB.movies.
 
-movieDB.movies.forEach((item, i) => {
-    moviesListBlock.insertAdjacentHTML('beforeend', 
-        `<li class="promo__interactive-item">
-            ${i + 1}. ${item}
-            <div class="delete"></div>
-        </li>`);
+    const submitAddingFilm = (e) => {
+        e.preventDefault();
+        let film = addFilmForm.querySelector('.adding__input').value,
+            isFavorite = addFilmForm.querySelector('input[type="checkbox"]').checked;
+
+        if (film.trim() !== '') {
+            // Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+            if (film.length > 21) {
+                film = `${film.slice(0, 21)}...`;
+            }
+            movieDB.movies = [...movieDB.movies, film];
+
+            // 4) Фильмы должны быть отсортированы алфавиту
+            showAllMovies(movieDB.movies, moviesListBlock);
+
+            // 4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+            // "Добавляем любимый фильм"
+            if (isFavorite) {
+                console.log('Добавляем любимый фильм');
+            }
+        }
+
+        e.target.reset();
+    };
+
+    addFilmForm.addEventListener('submit', submitAddingFilm);
+
 });
